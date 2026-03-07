df8105841c11ccb251d40ab7fec6fff6741ae570
Samitjainn <samitjain2020@gmail.com>
2026-03-06 18:51:33 -0800

    Add while loop implementation for sum of first n numbers
    Implement a while loop to calculate the sum of the first n numbers.

---
+++ sum_of_first_n.py
@@ -0,0 +1,9 @@
+#wap to find the sum of first n numbers using "while loop"  ............
+
+n=int(input("enter a number "))
+i=1
+sum=0
+while(i<=n):
+    sum=sum+i
+    i+=1
+print("sum of first n numbers",sum)  
